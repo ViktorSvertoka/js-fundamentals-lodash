dd9c9fc2e2d3c77c48ff1f040987d2bc19554a39
ViktorSvertoka <victor.svertoka@gmail.com>
2025-03-19 22:50:20 +0200

    feat(files): init project

---
+++ index.js
@@ -0,0 +1,25 @@
+import _ from "lodash";
+
+/*
+`getActiveUsers(users)`: Returns an array of active users.
+`getSortedUsersByAge(users)`: Returns an array of users sorted by age in ascending order.
+`getUserNames(users)`: Returns an array of all users' names.
+`getUserById(users, id)`: Returns the user object by the given id. If the user is not found, returns `null`.
+`addUser(users, newUser)`: Adds a new user to the array of users. Returns the updated array of users.
+`removeUserById(users, id)`: Removes the user by the given id. Returns the updated array of users.
+`updateUser(users, updatedUser)`: Updates the information of a user by their id. If the user is not found, the array of users remains unchanged.
+*/
+
+export function getActiveUsers(users) {}
+
+export function getSortedUsersByAge(users) {}
+
+export function getUserNames(users) {}
+
+export function getUserById(users, id) {}
+
+export function addUser(users, newUser) {}
+
+export function removeUserById(users, id) {}
+
+export function updateUser(users, updatedUser) {}
